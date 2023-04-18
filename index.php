4c5b673afa245433501892f3b53bb50befb7c941
Aranel9966 <99angelo.dire@gmail.com>
2023-04-18 14:23:05 +0200

    add thead

--- index.php
+++ index.php
@@ -49,36 +49,35 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-    
-    
-
     <table class="table table-dark table-striped-columns">
-    <?php
-    foreach($hotels as $hotel ){
-    ?>
-        <!-- <td> -->
-                <!-- <tr><td></td></tr> -->
+        <thead>
+                <?php
+                foreach($hotels[0] as $chiave => $valore){
+                ?>
+                    <th> <?php echo $chiave ?></th>
+                <?php
+                }
+                ?>
+        </thead>
+        <tbody>
+            <?php
+            foreach($hotels as $hotel ){
+            ?>
                 <tr>
-                <?php
-                foreach($hotel as $specifications){
-                ?>
-            <!-- </td>
-            <td>     -->
+                    <?php
+                    foreach($hotel as $specifications){
+                    ?>
                     <td>
-
-                <?php
-                echo "{$specifications}";
-                };
-                ?>
+                    <?php
+                    echo "{$specifications}";
+                    };
+                    ?>
                     </td>
-                </tr>
-
-            <!-- </td> -->
-    
-    <?php
-    };
-    ?>
-
+                </tr>    
+            <?php
+            };
+            ?>
+        </tbody>
     </table>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
